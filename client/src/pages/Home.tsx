c6fe2724064fecdffcec740ce995bc0d79946bf0
Step_atlas <stp.sip7@gmail.com>
2023-06-29 21:20:36 -0500

    stock

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+import { FormField, Loader } from "../components";
+
 export const Home = () => {
-  return <div></div>;
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="text-[32px] font-extrabold text-[#222328]">
+          The Community Showcase
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          The Community Showcase is a place for you to share your best work with
+          the world.
+        </p>
+      </div>
+      <div className="mt-16">
+        <FormField />
+      </div>
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>text here</>
+        )}
+      </div>
+    </section>
+  );
 };
