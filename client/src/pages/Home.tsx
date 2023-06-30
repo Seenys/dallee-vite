a83aa061bb7081988fac7dfd17f3932c93430dc8
Step_atlas <stp.sip7@gmail.com>
2023-06-30 15:50:00 -0500

    stock

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
-import { FormField, Loader } from "../components";
+import { Card, FormField, Loader } from "../components";
 
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post: any) => {
+      return <Card key={post._id} {...post} />;
+    });
+  }
+};
 export const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+
+  const [searchText, setSearchText] = useState("");
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -24,7 +34,15 @@ export const Home = () => {
             <Loader />
           </div>
         ) : (
-          <>text here</>
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#66e75] text-xl mb-3">
+                Showing results for{" "}
+                <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3"></div>
+          </>
         )}
       </div>
     </section>
